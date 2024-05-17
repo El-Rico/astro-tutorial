51a97f4b244bb74febd2a9448cf4619bbccc06f8
Rick Snackers <rick.snackers@apg.nl>
2024-05-17 15:58:56 +0200

    Completed tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
-	site: "https://rs-astro-tutorial.netlify.app",
+  site: "https://rs-astro-tutorial.netlify.app",
+  integrations: [preact()]
 });
